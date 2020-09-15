acd910448c12bb045e8df8dfe5f2f6b455004a61
Bucky Townsend <adam@desnudopenguino.com>
2020-09-15 04:26:40 +0000

    built a struct and added code to parse the json in and out of the response.

--- main.go
+++ main.go
@@ -2,18 +2,43 @@ package main
 
 import (
 	"fmt"
+	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+type BodyRequest struct {
+	RequestName string `json:"name"`
+}
+
+// BodyResponse is our self-made struct to build response for Client
+type BodyResponse struct {
+	ResponseName string `json:"name"`
+}
+
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	bodyRequest := BodyRequest{
+		RequestName: "",
+	}
 	//name := request.QueryStringParameters["name"]
-	name := request.Body
-	response := fmt.Sprintf("Hello %s", name)
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       response,
-	}, nil
+	err := json.Unmarshal([]byte(request.Body), &bodyRequest)
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 404}, nil
+	}
+
+	// We will build the BodyResponse and send it back in json form
+	bodyResponse := BodyResponse{
+		ResponseName: bodyRequest.RequestName + " LastName",
+	}
+
+	// Marshal the response into json bytes, if error return 404
+	response, err := json.Marshal(&bodyResponse)
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 404}, nil
+	}
+
+	//Returning response with AWS Lambda Proxy Response
+	return events.APIGatewayProxyResponse{Body: string(response), StatusCode: 200}, nil
 }
 
 func main() {
